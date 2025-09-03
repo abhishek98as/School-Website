39cb6738264c4f287c86328081f166c18f6cc34e
Shashank s singh <shashank98as@gmail.com>
2025-09-03 07:18:42 +0000

    I see this error with the app, reported by NextJS, please fix it. The er

--- next.config.ts
+++ next.config.ts
@@ -29,6 +29,12 @@ const nextConfig: NextConfig = {
         port: '',
         pathname: '/**',
       },
+      {
+        protocol: 'https',
+        hostname: 'photos.app.goo.gl',
+        port: '',
+        pathname: '/**',
+      }
     ],
   },
 };
